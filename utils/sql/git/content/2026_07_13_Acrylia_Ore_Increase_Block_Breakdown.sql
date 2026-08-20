bda93af4cdd8fa60505e45861d605146b6aac570
LictorTheEternal <42551049+LictorTheEternal@users.noreply.github.com>
2026-08-20 13:25:47 -0400

    Lictor the eternal acrylia ore breakdown increases (#373)
    * Create 2026_07_13_Acrylia_Ore_Increase_Large_Brick_Breakdown.sql
    
    This Creates a new row in tradeskill_recipe_entries table for the Recipe: Small Brick Of Acrylia Ore (Recipe ID 6095) which will return a second Small Brick Of Acrylia Ore when breaking down a Large Brick Of Acrylia Ore
    
    * Create 2026_07_13_Acrylia_Ore_Increase_Block_Breakdown.sql
    
    This Creates a new row in tradeskill_recipe_entries table for the Recipe: Large Brick Of Acrylia Ore (Recipe ID 5113) which will return a second Large Brick Of Acrylia Ore when breaking down a Block of Acrylia Ore

---
+++ utils/sql/git/content/2026_07_13_Acrylia_Ore_Increase_Block_Breakdown.sql
@@ -0,0 +1,2 @@
+/* This Creates a new row in tradeskill_recipe_entries table for the Recipe: Large Brick Of Acrylia Ore (Recipe ID 5113) which will return a second Large Brick Of Acrylia Ore when breaking down a Block of Acrylia Ore */
+INSERT INTO tradeskill_recipe_entries (id, recipe_id, item_id, successcount, failcount, componentcount, iscontainer) VALUES ( DEFAULT, '5113', '10424', '1', '0', '0', '0');
---
+++ utils/sql/git/content/2026_07_13_Acrylia_Ore_Increase_Large_Brick_Breakdown.sql
@@ -0,0 +1,2 @@
+/* This Creates a new row in tradeskill_recipe_entries table for the Recipe: Small Brick Of Acrylia Ore (Recipe ID 6095) which will return a second Small Brick Of Acrylia Ore when breaking down a Large Brick Of Acrylia Ore */
+INSERT INTO tradeskill_recipe_entries (id, recipe_id, item_id, successcount, failcount, componentcount, iscontainer) VALUES ( DEFAULT, '6095', '10423', '1', '0', '0', '0');
